54b167f73c038ef2e1f5402d1673617444289cc2
CindyChenx <chen_cx0128@hotmail.com>
2019-07-16 10:08:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+    return document.querySelector(selector);
+}
 
+function nestedTarget(){
+    return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+    let num = document.querySelectorAll('.ranked-list li');
+    for (let i = 0; i < num.length; i++){
+        num[i].innerHTML = parseInt(num[i].innerHTML) + n;
+    }
+}
+
+function deepestChild(){
+    return document.querySelector('#grand-node div div div div');
+}
