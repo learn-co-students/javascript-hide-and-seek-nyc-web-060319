bebc3f5d0c56d942e3c531466a3f1d3159693dba
te68 <te68@cornell.edu>
2019-07-17 11:11:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function  increaseRankBy(n) {
+  const lists = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i< lists.length; i++) {
+    //console.log(parseInt(lists[i].innerHTML) + n)
+    lists[i].innerHTML = (parseInt(lists[i].innerHTML) + n).toString()
+  }
+}
